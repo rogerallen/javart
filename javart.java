b64bea043ed9cf292d746d5b1224662f6ff61cb7
Roger Allen <rallen@gmail.com>
2018-05-24 12:10:29 -0700

    Chapter 2 done.

--- javart.java
+++ javart.java
@@ -6,12 +6,10 @@ public class javart {
         System.out.println(String.format("P3\n%d %d\n255",nx,ny));
         for(int j = ny-1; j >= 0; j--) {
             for(int i = 0; i < nx; i++) {
-                float r = (float)i / (float)nx;
-                float g = (float)j / (float)ny;
-                float b = 0.2f;
-                int ir = (int)(255.99*r);
-                int ig = (int)(255.99*g);
-                int ib = (int)(255.99*b);
+                vec3 col = new vec3((float)i/(float)nx, (float)j/(float)ny, 0.2f);
+                int ir = (int)(255.99*col.r());
+                int ig = (int)(255.99*col.g());
+                int ib = (int)(255.99*col.b());
                 System.out.println(String.format("%d %d %d",ir,ig,ib));
             }
         }
---
+++ vec3.java
@@ -0,0 +1,58 @@
+public class vec3 {
+    float[] e = new float[3];
+    public vec3() { }
+    public vec3(float e0, float e1, float e2) {
+        e[0] = e0; e[1] = e1; e[2] = e2;
+    }
+    public float x() { return e[0]; }
+    public float y() { return e[1]; }
+    public float z() { return e[2]; }
+    public float r() { return e[0]; }
+    public float g() { return e[1]; }
+    public float b() { return e[2]; }
+
+    // no operator overloading for java.
+    public vec3 neg() {
+        return new vec3(-e[0], -e[1], -e[2]);
+    }
+    public vec3 add(vec3 v2) {
+        return new vec3(e[0] + v2.e[0], e[1] + v2.e[1], e[2] + v2.e[2]);
+    }
+    public vec3 sub(vec3 v2) {
+        return new vec3(e[0] - v2.e[0], e[1] - v2.e[1], e[2] - v2.e[2]);
+    }
+    public vec3 mul(vec3 v2) {
+        return new vec3(e[0] * v2.e[0], e[1] * v2.e[1], e[2] * v2.e[2]);
+    }
+    public vec3 mul(float t) {
+        return new vec3(e[0] * t, e[1] * t, e[2] * t);
+    }
+    public vec3 div(vec3 v2) {
+        return new vec3(e[0] / v2.e[0], e[1] / v2.e[1], e[2] / v2.e[2]);
+    }
+    public vec3 div(float t) {
+        return new vec3(e[0] / t, e[1] / t, e[2] / t);
+    }
+    public float length() {
+        return (float)Math.sqrt(e[0]*e[0]+e[1]*e[1]+e[2]*e[2]);
+    }
+    public double squared_length() {
+        return e[0]*e[0]+e[1]*e[1]+e[2]*e[2];
+    }
+    public void make_unit_vector() {
+        float k = 1.0f/length();
+        e[0] *= k; e[1] *= k; e[2] *= k;
+    }
+
+    public static float dot(vec3 v1, vec3 v2) {
+        return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+    }
+    public static vec3 cross(vec3 v1, vec3 v2) {
+        return new vec3( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
+			-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0]),
+			 (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
+    }
+    public static vec3 unit_vector(vec3 v){
+        return v.div(v.length());
+    }
+}
